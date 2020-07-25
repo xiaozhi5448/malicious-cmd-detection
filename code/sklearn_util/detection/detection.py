13b7f66f94302f9d8bcadb66397f136d8bba36bd
xiaozhi5448 <silenceaxx@gmail.com>
2020-07-25 19:43:28 +0800

    mod path

--- code/sklearn_util/detection/detection.py
+++ code/sklearn_util/detection/detection.py
@@ -1,16 +1,21 @@
+
 import numpy as np
 from sklearn.ensemble import IsolationForest
 import random
-from data.util import load_data
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics import classification_report
 from sklearn.svm import OneClassSVM
 from sklearn.neighbors import LocalOutlierFactor
+import sys
+import os
+sys.path.append(os.getcwd())
+
 
 def test_lof(X, Y):
     clf = LocalOutlierFactor(n_neighbors=20, contamination=0.19)
     y_pred = clf.fit_predict(X)
     print(classification_report(Y, y_pred))
+
 
 def test_ocs(X, Y, normal_vecs, abnormal_vecs):
 
@@ -19,15 +24,19 @@ def test_ocs(X, Y, normal_vecs, abnormal_vecs):
     y_pred = clf.predict(X)
     print(classification_report(Y, y_pred))
 
+
 def test_iforest(X, Y):
 
-    clf  = IsolationForest(max_samples=5000, random_state=np.random.RandomState(42), contamination=0.19)
+    clf = IsolationForest(
+        max_samples=5000, random_state=np.random.RandomState(42), contamination=0.19)
     clf.fit(X)
     y_pred = clf.predict(X)
     print(classification_report(Y, y_pred))
     print(y_pred)
 
+
 def test():
+    from data.util import load_data
     normal_data_set, abnormal_data = load_data()
     normal_data = normal_data_set
     data = []
@@ -51,6 +60,7 @@ def test():
     test_iforest(X, Y)
     print("result for LOF")
     test_lof(X, Y)
+
 
 if __name__ == '__main__':
     test()
